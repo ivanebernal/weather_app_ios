f1d32aef678eed16b01a141195541b892ba92320
Ivan Esparza <ivanebernal@gmail.com>
2019-01-29 12:43:06 -0700

    Initial Commit

---
+++ Weather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by Ivan Esparza on 1/29/19.
+//  Copyright © 2019 ivanebernal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
